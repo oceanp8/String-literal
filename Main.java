4bf3b7345844c0e16662c26873cf649e0f3e42e4
oceanp8 <148895610+oceanp8@users.noreply.github.com>
2024-09-08 01:09:34 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("I like food");
+        System.out.println();
+        System.out.println("Why was summer so short");
+    }
+}
